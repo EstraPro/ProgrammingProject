2ea5efd8d9fc547b9b82e65dc8415fc155bc682f
jauregi13 <jauregi13@jon.j1305@gmail.com>
2020-10-15 13:42:19 +0200

    change buffer to Scanner

--- ProgrammingProject/src/Main.java
+++ ProgrammingProject/src/Main.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
 	
@@ -11,47 +12,28 @@ public class Main {
 		String fileName = "files/peopleG612056.txt";	//**Must be able to load several people files!!
 
 		try {
-	         //BufferedReader creation
-	         BufferedReader br = new BufferedReader(new FileReader(fileName));
+	         //Scanner creation
+	         Scanner scan = new Scanner(new FileReader(fileName));
 	         //Leer las 2 primeras línea, guardando en un String
-	         String texto = br.readLine();
+	         scan.readLine();
 	         texto = br.readLine();
 	         //Repetir mientras no se llegue al final del fichero
 	        
-	         String p1= new String(texto);
-	         
-	         //Leer la siguiente línea
-             texto = br.readLine();
-             
-             String p2= new String(texto);
-             
-             //Leer la siguiente línea
-             texto = br.readLine();
-             
-             String p3= new String(texto);
-             
-             //Leer la siguiente línea
-             texto = br.readLine();
-             
-             String p4= new String(texto);
-             
-             System.out.println(p1);
-             System.out.println(p2);
-             n1.addPeople(p1);
-             
-	         while(texto != null)
-	         {
+	         while(scan.hasNext()){
 	        	 
-	         }
+	        	 String p= new String(texto);
+	        	 n1.addPeople(p);
+	        	 System.out.println(p.toString());
+	         }      
 	         
 		}
-		catch (FileNotFoundException a) {
+		/*catch (FileNotFoundException a) {
 	          System.out.println("Error: Fichero no encontrado");
 	          System.out.println(a.getMessage());
 	          }
 	    catch(Exception e) {
 	          System.out.println("Error de lectura del fichero");
 	          System.out.println(e.getMessage());
-	    	  }
+	    	  }*/
 	}
 }
